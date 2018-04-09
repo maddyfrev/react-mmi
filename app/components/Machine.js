0c8b96450a26b1e4481e96457035a1528b2d0a87
Maddy Freville <maddy.freville@etu.univ-grenoble-alpes.fr>
2018-04-09 06:26:46 +0000

    Création du composant Machine

--- app/components/Machine.js
+++ app/components/Machine.js
@@ -1,3 +1,34 @@
+
+/**
+ *
+ * Machine.js
+ *
+ */
+ /*
+import React from 'react';
+
+class Machine extends React.Component {
+   handleClick() { console.log("coucou")}
+   render() {
+  // Dans tous les cas, afficher
+  //this.props.isActive? "machine" : "machine active"
+  //y'a du potentiel
+      return (
+    <div 
+    className={this.props.isActive ? "machine active" : "machine"}>
+    <h2> {this.props.name} </h2>
+     <button onClick={(e) => this.handleClick(e)} type="button" className="btn">
+     Toggle
+    </button>
+      
+    </div>
+    )
+  }
+}
+// Le composant sera accessible avec le nom "Machine"
+export default Machine;
+*/
+
 /**
  *
  * App.js
@@ -6,20 +37,38 @@
 import React from 'react';
 
 class Machine extends React.Component {
-   handleClick() { console.log('Hello')}
-   render() {
-  // Dans tous les cas, afficher
-      return (
+
+    
+
+  
+  render() {
+
+    // Dans tous les cas, afficher
+    return ( 
     <div 
-    className={this.props.isActive ? "machine active" : "machine"}>
-    <h2> {this.props.name} </h2>
-     <button onClick={this.handleClick} type="button" className="btn">
-     Toggle
+    className={this.props.isMachineOn ? "machine active" : "machine"}>
+    <h2>{this.props.name}</h2>
+    <button onClick={this.props.handleClick}>
+        {this.props.isMachineOn ? 'ON' : 'OFF'}
     </button>
-      
+    
     </div>
+    
     )
   }
 }
+
+
+//explication de la ligne 12: "?" est équivalent à un "if" et ":" est équivalent à un "else"
+//ligne 12: si isActive n'est pas vide dans inedx.js, la div a comme classe "machine-active", sinon elle a comme classe "machine-desactivee"
+
+//ligne 15: <button onClick = {changeClass}>Changer la classe</button>
+
+//dans index.js:
+/*  <Machine title = "1" abcde = "Premiere machine" isActive = "Yes" />
+    <Machine title = "2" abcde = "Deuxieme machine" isActive = "No"/>
+    <Machine title = "3" abcde = "Troisieme machine" isActive = ""/>
+*/
+
 // Le composant sera accessible avec le nom "Machine"
 export default Machine;
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -3,6 +3,8 @@
  * App.js
  *
  */
+/* 
+
 import React from 'react';
 
 // On importe ici les composants qu'on veut afficher
@@ -28,7 +30,7 @@ import '../../css/style.css';
     </div>
   );
 }*/
-
+/*
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -82,4 +84,101 @@ class App extends React.Component {
   );
 }
 }
-export default App;  
+
+  */
+  
+  /**
+ *
+ * App.js
+ *
+ */
+import React from 'react';
+import Header from '../../components/Header.js';
+import Footer from '../../components/Footer.js';
+import Machine from '../../components/Machine.js';
+import '../../css/style.css';
+
+
+class app extends React.Component {
+  
+  constructor(props) {
+    super(props); //super(props) permet d'avoir accès au props
+    this.state = {
+    machines:[
+          {               
+            id: 0,   
+            isMachineOn: true,
+            name: "Machine à soda"
+             },
+             
+          {               
+            id: 1, 
+            isMachineOn: false,
+            name: "Machine à café"
+             },
+             
+          {         
+            id: 2, 
+            isMachineOn: false,
+            name: "Machine à cappucino"
+             },
+             
+        {            
+            id: 3, 
+            isMachineOn: true,
+            name: "Machine à thé"
+             },
+        ]
+};
+
+    console.log(this.state);
+  
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+        handleClick() {
+    this.setState(prevState => (
+        {isMachineOn: !prevState.isMachineOn}
+        ));
+    }
+    
+
+  
+  
+ 
+  
+    render(){ 
+
+     return(
+         
+    <div>
+
+    <Header/>
+    
+    <button onClick={this.handleClick}>
+     {this.state.isMachineOn ? 'ON' : 'OFF'}
+    </button>
+    { this.state.machines.map(machine =>
+                  //console.log(machine.name))}
+    
+        <Machine name= {machine.name}
+                key={machine.id}
+         isMachineOn={machine.isMachineOn}
+        />
+        
+    )}
+    
+    <Footer/>
+    </div>
+    
+  );
+}
+}
+//quand on indique "props", on appelle une valeur qui vient généralement d'autre part. (un autre fichier, un truc parent...)
+//quand on indique "state", c'est pour changer une valeur en local. d'où l'importante d'indiquer state dans le div et le button de ce component.
+//et de définir ce qu'il y a dans le state (ligne 12)
+// Le composant sera accessible avec le nom "ThisMachine"
+export default app;
